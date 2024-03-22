744d596d1907dc68cf02e39cc34a7fa530a60b88
ishigo1987 <stephnguets@yahoo.fr>
2024-03-22 13:50:45 +0100

    Delete back-end composants

--- Back-end/src/app.ts
+++ Back-end/src/app.ts
@@ -1,36 +1,4 @@
-"use strict";
 
-let nodeProcess: { [key: string]: any };
+import process from "node:process";
 
-nodeProcess = require("node:process");
-
-const PORT:string = nodeProcess?.env?.PORT;
-
-// const server = require("./modules/createHttpServer.ts")();
-
-// const httpServer = require("./modules/httpServer.ts");
-
-
-/* before the server initialization we load the page index.html */
-
-// const loadHtmlPages = require("./modules/loadHtmlPages.ts");
-
-// loadHtmlPages().then((response)=>{
-
-//      global.indexHtml = response[0]; // index.html
-
-//      global.homeHtml = response[1]; // home.html
-
-//      global.notFoundHTML = response[2]; // not-found html
-
-//      server.on("request", httpServer);
-
-//      return server.listen(PORT);  
-
-// });
-
-// server.on('timeout', (msg)=>{
-//    // console.log("Timeout check the message argument dumb ass")
-// });
-
-
+// const PORT:string = process?.env?.PORT;
